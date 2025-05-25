a5b763a26d1f85ace53cdba8aee6611b97e3ab1f
Danu Alamsyah Putra <152958424+DanuAlamsyah@users.noreply.github.com>
2025-05-25 17:40:05 +0700

    tilik

--- app/Http/Controllers/Api/TilikController.php
+++ app/Http/Controllers/Api/TilikController.php
@@ -11,7 +11,7 @@ use Exception;
 class TilikController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource
      *
      * @return \Illuminate\Http\JsonResponse
      */
